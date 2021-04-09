58eed6262eb80792a8d6351bdd0ed42f2870667e
FrederickMih <fredtimermih@gmail.com>
2021-04-09 08:08:51 +0100

    Remove comments from code

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,6 @@ class User < ApplicationRecord
   has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
-  # def friends
-  #   friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-  #   friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-  #   friends_array.compact
-  # end
-
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
